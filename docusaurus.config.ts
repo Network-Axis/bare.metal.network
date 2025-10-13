e3347dbd44f72fd15f99c1065638c757bb54633b
Steven Cassamajor <contributor@networkaxis.org>
2025-10-13 00:01:12 -0400

    Update themeConfig.image as well

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -74,7 +74,7 @@ const config: Config = {
   
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/logo.svg',
+    image: 'img/logo.png',
     navbar: {
       title: 'Bare Metal Networking for Private Clouds',
       logo: {
